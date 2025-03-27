011983bb140ca067468ab592cff1cd6b9846be33
ManuelagDuque <grajalesduquemanuela@gmail.com>
2025-03-27 10:12:15 +0100

    fiizBuzz.js file added

---
+++ src/scripts/fizzBuzz.js
@@ -0,0 +1,12 @@
+/**
+ * Responsabilidad: gestionar la lógica de FizzBuzz
+ * Recuerda que la estructura de las respuestas debe ser estructurada:
+ *  {
+        status: "", // Código indicando éxito o error
+        message: "", // Mensaje
+        data: {
+            number: 0, // Número evaluado
+            result: "" // Resultado esperado
+            }
+    }
+ * */ 
