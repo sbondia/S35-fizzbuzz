e7d2f49b40256d6b767ee8f98f26981264af1d66
Sergi Bondia <sbondia716@gmail.com>
2025-05-26 12:46:02 +0200

    boton por evento

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -1,12 +1,9 @@
 /**
  * Responsabilidad: gestionar los eventos
  * */ 
-/*
+
 const btn = document.getElementById("btn")
-console.log(btn)
 btn.addEventListener("click", (click) => {
-	console.log("hola")
 	click.preventDefault();
-	fizzBuzz(domInput())
+	domInput()
 })
-*/
